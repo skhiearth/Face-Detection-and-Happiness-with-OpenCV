77961672166004ac42f4e5044cab2e2f0ba4e136
skhiearth <skhiearth@gmail.com>
2019-11-07 02:41:15 +0530

    add outputs

--- faceDetectionwithOpenCV.py
+++ faceDetectionwithOpenCV.py
@@ -3,11 +3,12 @@
 
 # Import OpenCV
 import cv2
+import time
 
 
 # Loading the cascades
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('/Users/skhiearth/Desktop/face/haarcascade_frontalface.xml')
+eye_cascade = cv2.CascadeClassifier('/Users/skhiearth/Desktop/face/haarcascade_eye.xml')
 
 
 # Face Detector
@@ -31,19 +32,22 @@ def face_detector(grayScale, originalFrame):
 
 
 # Accessing webcam to detect face
-video_capture = cv2.VideoCapture(0) 
+video_capture = cv2.VideoCapture(0)
+print('Detecting camera...')
+time.sleep(20)
 
-while True: 
+while(video_capture.isOpened()):
     re, frame = video_capture.read()
-    
+
     if re:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         canvas = face_detector(gray, frame)
         cv2.imshow('Video', canvas)
-    
+    else:
+        print('No camera detected.')
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-video_capture.release() 
-cv2.destroyAllWindows() # We destroy all the windows inside which the images were displayed.
-
+video_capture.release()
+cv2.destroyAllWindows()
