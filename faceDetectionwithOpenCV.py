18507ab3f21132af7afe3503d78e388082e5ce1a
skhiearth <skhiearth@gmail.com>
2019-11-07 03:32:25 +0530

    smile detection

--- faceDetectionwithOpenCV.py
+++ faceDetectionwithOpenCV.py
@@ -7,9 +7,9 @@ import time
 
 
 # Loading the cascades
-face_cascade = cv2.CascadeClassifier('/Users/skhiearth/Desktop/face/haarcascade_frontalface.xml')
-eye_cascade = cv2.CascadeClassifier('/Users/skhiearth/Desktop/face/haarcascade_eye.xml')
-
+face_cascade = cv2.CascadeClassifier('/Users/skhiearth/Desktop/Face-Detection-and-Happiness-with-OpenCV/haarcascade_frontalface.xml')
+eye_cascade = cv2.CascadeClassifier('/Users/skhiearth/Desktop/Face-Detection-and-Happiness-with-OpenCV/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('/Users/skhiearth/Desktop/Face-Detection-and-Happiness-with-OpenCV/haarcascade_smile.xml')
 
 # Face Detector
 def face_detector(grayScale, originalFrame):
@@ -21,12 +21,20 @@ def face_detector(grayScale, originalFrame):
                       (255, 255, 0), 3) # Drawing rectangle over face on Original Frame
         roi_gray = grayScale[y1:y1+h1, x1:x1+w1]
         roi_color = originalFrame[y1:y1+h1, x1:x1+w1]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3) # Coordinates of detected eyes
+        
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 21) # Coordinates of detected eyes
         for (x2, y2, w2, h2) in eyes: # For loop to iterate over detected eyes
             cv2.rectangle(roi_color,
                           (x2, y2), # Upper Left Corner
                           (x2+w2, y2+h2), # Lower Right Corner
-                          (255, 0, 0), 1) # Drawing rectangle over eye on Original Frame
+                          (255, 0, 0), 2) # Drawing rectangle over eye on Original Frame
+                          
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22) # Coordinates of detected smile
+        for (sx, sy, sw, sh) in smiles: # For loop to iterate over detected smile
+            cv2.rectangle(roi_color,
+                          (sx, sy), # Upper Left Corner
+                          (sx+sw, sy+sh), # Lower Right Corner
+                          (255, 0, 0), 2) # Drawing rectangle over eye on Original Frame
 
         return originalFrame
 
@@ -46,7 +54,7 @@ while(video_capture.isOpened()):
     else:
         print('No camera detected.')
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(30) & 0xFF == ord('q'):
         break
 
 video_capture.release()
